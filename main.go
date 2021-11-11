4285666e1a878ec4fc4b801d5be5162bea704cd8
Bannasarn Boonmee <bannasarn.boonmee@hotmail.com>
2021-11-11 13:55:44 +0700

    Move oscar exercise to folder

--- main.go
+++ main.go
@@ -1,39 +1,9 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
-	"os"
+	"github.com/bannasarn/go-hello/oscar"
 )
 
 func main() {
-	exerciseOscar()
-}
-
-func exerciseOscar() {
-	// Open File
-	f, err := os.Open("/Users/bannasarn/Downloads/oscar.csv")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Read File
-	r := csv.NewReader(f)
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Map record into name
-	name := make(map[string]int)
-	for _, record := range records {
-		name[record[3]] += 1
-	}
-
-	// Filter value more than 1
-	for k, v := range name {
-		if v > 1 {
-			println(k)
-		}
-	}
+	oscar.Oscar()
 }
---
+++ oscar/oscar.go
@@ -0,0 +1,37 @@
+package oscar
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+)
+
+// Find the name who get the oscar more than one time
+
+func Oscar() {
+	// Open File
+	f, err := os.Open("./oscar/oscar.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Read File
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Map record into name
+	name := make(map[string]int)
+	for _, record := range records {
+		name[record[3]] += 1
+	}
+
+	// Filter value more than 1
+	for k, v := range name {
+		if v > 1 {
+			println(k)
+		}
+	}
+}
